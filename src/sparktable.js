4124a768fbecf81ac60c436bffa2b0de1a89a614
Cory Forsyth <cory.forsyth@gmail.com>
2013-08-01 18:19:19 -0400

    Basic adding of spans to table tis

--- src/sparktable.js
+++ src/sparktable.js
@@ -8,31 +8,114 @@
 
 (function($) {
 
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  var defaultOpts = {
+    /* the selector to use to find elements from the table */
+    tdSelector: 'td',
+
+    /* verifies that we will include this el in the collection */
+    tdVerifierFn: function(){ return true; },
+
+    /* gets the max of the given values */
+    maximizerFn: function(val1, val2) {
+      return Math.max(val1, val2);
+    },
+
+    /* finds the numeric value from the given el's html */
+    valuefierFn: function( el ) {
+      console.log(el);
+      var html = $(el).html();
+      console.log('html',html,'innerHTML',$(el).innerHTML);
+      var res = parseInt(html, 10);
+      console.log('string',html,res);
+      return res;
+    },
+
+    /* returns a percentage (float) from the val, max and min */
+    percentageFn: function(val, max, min) {
+      if (!min) { min = 0; }
+
+      var res = Math.round( 100 * (val - min) / (max - min) );
+      return res;
+    },
+
+    /* input: percentage, returns a span representing that percentage */
+    decoratorElFn: function(percentage) {
+      return "<span class='sparktable-percentage'>" +
+             percentage +
+             "</span>";
+    }
+  };
+
+
+  /* iterates the sourceEl to find all the valid sub items */
+  var getCollection = function(sourceEl,
+                             tdSelector,
+                             tdVerifierFn) {
+    var items = [], item;
+    sourceEl.find(tdSelector).each(function(){
+      item = $(this);
+      if (tdVerifierFn(item)) { items.push(item); }
     });
+
+    return items;
   };
 
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
+  /* iterates the collection and returns the maximum value */
+  var getMaxValue = function( collection, valuefierFn, maximizerFn){
+    var maxValue, value;
+
+    for (var i = 0; i < collection.length; i++) {
+      value = valuefierFn(collection[i]);
+
+      if (!maxValue) {
+        maxValue = value;
+      } else {
+        maxValue = maximizerFn( maxValue, value );
+      }
+    }
+
+    return maxValue;
   };
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
+  var decorateCollection = function(collection,
+                                    maxValue,
+                                    valuefierFn,
+                                    percentageFn,
+                                    decoratorElFn ) {
+    var item, value, percentage;
+
+    for (var i = 0; i < collection.length; i++) {
+      item  = collection[i];
+      value = valuefierFn(item);
+      percentage = percentageFn(value, maxValue);
+
+      $(item).append( decoratorElFn(percentage) );
+    }
   };
 
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
+
+  // Collection method.
+  $.fn.sparktable = function(opts) {
+    var collection = [],
+        maxValue;
+
+    opts = $.extend({}, defaultOpts, opts || {});
+
+    collection = getCollection($(this),
+                               opts.tdSelector,
+                               opts.tdVerifierFn );
+    maxValue = getMaxValue( collection,
+                            opts.valuefierFn,
+                            opts.maximizerFn );
+
+    decorateCollection( collection,
+                        maxValue,
+                        opts.valuefierFn,
+                        opts.percentageFn,
+                        opts.decoratorElFn );
+
+
+    return this;
   };
 
 }(jQuery));
--- test/sparktable_test.js
+++ test/sparktable_test.js
@@ -20,12 +20,53 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
+  var tableHTML = "<table id='table'>" +
+    "<tr>" + "<th>head1</th>" + "<th>head2</th>" + "</tr>" +
+    "<tr>" + "<td>1</td>"      + "<td>4</td>"     + "</tr>" +
+    "</table>";
+
+  var includesText = function(string, includedText){
+    ok( string.indexOf(includedText) > -1,
+        "Expected '" + string + "' to include '" + includedText + "'.");
+  };
+
+  var elementIncludesText = function(selector, includedText){
+    return includesText( $(this.table).find(selector).html(),
+                         includedText );
+  };
+
+  module('jQuery#sparktable basics', {
     // This will run before each test in this module.
     setup: function() {
-      this.elems = $('#qunit-fixture').children();
+      $('#qunit-fixture').append(tableHTML);
+      this.table = $('#table');
+      $(this.table).sparktable();
+    },
+    teardown:function(){
+      $('#table').remove();
     }
   });
+
+  test('finds the table', function(){
+    ok(this.table.length);
+  });
+
+  test('changes the values of the tds', function(){
+    elementIncludesText( 'td:eq(0)', '1');
+  });
+
+  test('adds a span to the tds', function(){
+    elementIncludesText( 'td:eq(0)', '<span');
+    elementIncludesText( 'td:eq(0)', '/span>');
+  });
+
+  test('includes the percentage in the tds', function(){
+    elementIncludesText('td:eq(0)', '25');
+    elementIncludesText('td:eq(1)', '100');
+  });
+
+
+  /*
 
   test('is chainable', function() {
     expect(1);
@@ -58,5 +99,6 @@
     // Use deepEqual & .get() when comparing jQuery objects.
     deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
   });
+  */
 
 }(jQuery));
